5411695a838c1b3e1dabc14b025b93025d4d23a6
lviardcretat <loic.viard-cretat@proton.me>
2024-07-31 15:18:46 +0200

    Added ags typescript config

--- modules/gui/ags/ags.nix
+++ modules/gui/ags/ags.nix
@@ -10,6 +10,12 @@
     # null or path, leave as null if you don't want hm to manage the config
     configDir = ./config;
     # additional packages to add to gjs's runtime
-    extraPackages = with pkgs; [];
+    extraPackages = with pkgs; [
+      bun
+    ];
   };
+
+  home.packages = with pkgs; [
+    bun
+  ];
 }
